4becdd64e2ba8e3f2726b6ffac300200ecc61890
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-01-11 12:09:28 -0500

    commit tree 25ce86d75a92664ea6a79580f95cc9a56aa72d54

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+   cats.unshift("Bob"); 
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Ralph");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Bob");
+}
+
+function appendCat() {
+    const cats2 = [...cats, "Broom"];
+    return cats2;
+}
+
+function prependCat() {
+    const cats3 = ["Arnold", ...cats];
+    return cats3;
+}
+
+function removeLastCat() {
+    return cats.slice(0, 2);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
